75fe08369a657593620cd0cd64db18f5684c391d
bev <bevan.lee@live.com.au>
2022-06-19 11:44:46 +1000

    generalise the inputs to the program; produce csv style output

--- main.py
+++ main.py
@@ -1,41 +1,73 @@
 import pytesseract
 from PIL import Image
 import sys
+import re
 
+#use member_number as starting point for costco receipt
 f = open("member_number", "r")
-
 MEMBER_NUMBER = f.read()
-TESSERACT_PATH = r"D:\\tesseract\\tesseract"
 f.close()
 
-print(MEMBER_NUMBER)
+START_STRING = MEMBER_NUMBER
+# PATH to tesseract executable
+TESSERACT_PATH = r"D:\\tesseract\\tesseract"
+WHITELIST_LETTERS = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,-_/"
 
 
-# def read_image(string, member_number):
-#     read = False
-#     for line in string.split("\n\n"):
+def read_image(string, member_number, filename):
+    reading_items = False
+    string = string.replace("\n\n", "\n")
+    string_list = string.split('\n')
+    
+    items = [""] * (len(string_list)//2)
+    # a bit larger than needed as we ignore first and last few lines
+    curr = 0
+    i = 0
+    print(len(string_list))
+    while i < len(string_list):
         
+        if "COMMONWEALTH" in string_list[i] or "COMMONWEALTH" in string_list[i-1]:
+            reading_items = False
+        if reading_items:
+            items[curr] = string_list[i] + ", " + string_list[i+1].replace(" ", ",")
+            #print(items[curr])
+            curr += 1
+            i += 1
+        if str(member_number) in string_list[i]:
+            reading_items = True
+        i += 1
+    with open("items/" + filename, "w") as f:
+        for item in items:
+            f.write(item+"\n")
+
+if __name__ == "__main__":
+    pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+
+    # Format receipt output to csv
+    if len(sys.argv) == 2:
+
+        f = open("logs/" + sys.argv[1], "r")
+        read_image(f.read(), START_STRING, sys.argv[1])
+        f.close()
+
+    # Perform OCR on receipt image
+    elif len(sys.argv) == 3:
+        try:
+            if sys.argv[1] == '-p':
+                
+                pdf = pytesseract.image_to_pdf_or_hocr(sys.argv[2], extension='pdf')
+                with open('test.pdf', 'w+b') as f:
+                    f.write(pdf) # pdf type is bytes by default
+
+            elif sys.argv[1] == '-t':
+                out = pytesseract.image_to_string(Image.open(sys.argv[2]), lang='eng')
+                print(out)
+
+                with open("logs/" + re.split('[./]',sys.argv[2])[-2], "w") as f:
+                    f.write(out)
+        except:
+            print("exception")
+    else:
+        print()
         
-#         if read:
-#             print(line)
-#         if str(member_number) in line:
-#             read = True
-#         if "COMMONWEALTH" in line:
-#             read = False
-
-# if __name__ == "__main__":
-#     pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
-
-#     if len(sys.argv) == 2:
-
-#         f = open("r1", "r")
-#         read_image(f.read(), MEMBER_NUMBER)
-#     elif len(sys.argv) == 3:
-#         print(sys.argv)
-#     else:
-        
-#     # out = pytesseract.image_to_string(Image.open('r1.jpg'), lang='eng')
-#     # f = open("r1", "x")
-#     # f.write(out)
-#     # print(out)
-#     # f.close()
+    
